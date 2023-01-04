d7bca5b5fa906af704ee1fd2279d304c1d093c72
nitin-pandita <nitin_207013@saitm.org>
2023-01-04 22:14:30 +0530

    Function 1 Program for Return value

---
+++ Functions/src/Functions/Function1.java
@@ -0,0 +1,40 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class Function1 {
+    public static void main(String[] args) {
+        sum();
+    }
+    // return the value
+//    int + int  = int
+    static int sum2(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first Number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the Second Number: ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        return sum;
+    }
+    static void sum(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first Number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the Second Number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is :"+sum);
+    }
+// Syntax for functions
+    /*
+        return_type name() {
+        //body
+        return statement;
+        }
+    */
+}
