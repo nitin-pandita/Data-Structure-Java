a4a7f44a0110962f1876dbb8a4011db8524a1d86
nitin-pandita <nitin_207013@saitm.org>
2023-01-07 14:20:48 +0530

    Prime Number Program

---
+++ Functions/src/Functions/PrimeNumber.java
@@ -0,0 +1,27 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+    }
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0) {
+                return false;
+            }
+            c++;
+
+        }
+        return c * c > n;
+
+    }
+}
