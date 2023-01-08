53b576ebb435443351613a23ba44648a383fe2bc
nitin-pandita <nitin_207013@saitm.org>
2023-01-08 18:42:37 +0530

    Array introduction

---
+++ Array/src/ArrayIntro.java
@@ -0,0 +1,14 @@
+public class ArrayIntro {
+    public static void main(String[] args) {
+//        int [] arr = new int[5];
+        //syntax
+        // datatype [] variable_name = new datatype [size];
+
+        // STORE rollno of 5 students
+//        int [] arr2 = {345,56,76,34,34};
+        // [] represents the data stored is of array type;
+
+        int [] arr1; // declaration of array. arr1 is getting defined in the stack
+        arr1 = new int[5]; // initialisation : actually here object is being created in the heap memory
+    }
+}
