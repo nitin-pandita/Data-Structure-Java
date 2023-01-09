e0c7170761235ce07081edd61675a3da87fcb915
nitin-pandita <nitin_207013@saitm.org>
2023-01-09 15:50:53 +0530

    input java array

--- Array/src/Input.java
+++ Array/src/Input.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Input {
@@ -21,9 +22,17 @@ public class Input {
 
 
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println("At index "+i+" value is " +arr[i]);
-            System.out.println("Thank you");
-        }
+        System.out.println(Arrays.toString(arr));
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println("At index "+i+" value is " +arr[i]);
+//        }
+
+        //                   For Each Loop
+//        for (int num: arr) //here num represents the element of the array
+//
+//        {
+//            System.out.println(num + " ");
+//        }
+//        System.out.println(arr[5]); // Array out of Bound
     }
 }
