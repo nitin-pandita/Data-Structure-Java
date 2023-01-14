28834f8b3ae9dd507954af03fa54ef052a3c3673
nitin-pandita <nitin_207013@saitm.org>
2023-01-14 16:54:57 +0530

    InsertElement second Method Program Java

--- Array/Array_Assignment/src/InsertElement.java
+++ Array/Array_Assignment/src/InsertElement.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,14 +10,27 @@ public class InsertElement {
         System.out.println("Enter the Element to be inserted: ");
         int element = sc.nextInt();
 
-        int [] arr = {34,234,546,23,67};
-        int n = arr.length;
-        int [] newArr = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            newArr[i] = arr[i];
-        }
-        newArr[n] = element; // large array and added the element to it
-        System.out.println(Arrays.toString(newArr));
+        Integer [] arr = {34,234,546,23,67};
+//   -------    Method first ----------
+
+//        int n = arr.length;
+//        int [] newArr = new int[n + 1];
+//        for (int i = 0; i < n; i++) {
+//            newArr[i] = arr[i];
+//        }
+//        newArr[n] = element; // large array and added the element to it
+//        System.out.println(Arrays.toString(newArr));
+
+        // Method Two is ArrayList
+
+        ArrayList<Integer> mylist = new ArrayList<Integer>(Arrays.asList(arr)); // converting array into list
+
+        mylist.add(element);
+
+        arr = mylist.toArray(arr); // now converting list to array
+
+        System.out.println(Arrays.toString(arr)); // printing the array
     }
+
 
 }
