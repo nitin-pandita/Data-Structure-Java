52ff4173a8307b7cc7cdfa4a8ee8f5eaba4af391
nitin-pandita <nitin_207013@saitm.org>
2023-01-23 00:08:53 +0530

    updated

---
+++ Searching/BinarySearch/src/Demo1.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int [] arr = {5,7,7,8,8,10};
+        int target = 8;
+        System.out.println(Arrays.toString(searchRange(arr,target)));
+    }
+    static int[] searchRange(int[] nums, int target) {
+        int [] ans = {-1,-1};
+        int start = search(nums,target,true);
+        int end = search(nums,target,false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+
+    static int search(int [] nums, int target, boolean Leftside){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length -1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(target < nums[mid]){
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            }else{
+                ans = mid;
+                if(Leftside){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
