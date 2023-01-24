07980489ba8b41190724917560a30f34c582d36f
nitin-pandita <nitin_207013@saitm.org>
2023-01-24 15:11:07 +0530

    Demo 1

--- Searching/BinarySearch/src/Demo1.java
+++ Searching/BinarySearch/src/Demo1.java
@@ -2,41 +2,57 @@ import java.util.Arrays;
 
 public class Demo1 {
     public static void main(String[] args) {
-        int [] arr = {5,7,7,8,8,10};
-        int target = 8;
-        System.out.println(Arrays.toString(searchRange(arr,target)));
+        int [] arr = {5,7,9,18,28,30};
+        int target = 18;
+        System.out.println((floor(arr,target)));
     }
-    static int[] searchRange(int[] nums, int target) {
-        int [] ans = {-1,-1};
-        int start = search(nums,target,true);
-        int end = search(nums,target,false);
+//    static int[] searchRange(int[] nums, int target) {
+//        int [] ans = {-1,-1};
+//        int start = search(nums,target,true);
+//        int end = search(nums,target,false);
+//
+//        ans[0] = start;
+//        ans[1] = end;
+//
+//        return ans;
+//    }
+//
+//    static int search(int [] nums, int target, boolean Leftside){
+//        int ans = -1;
+//        int start = 0;
+//        int end = nums.length -1;
+//        while(start <= end){
+//            int mid = start + (end - start)/2;
+//            if(target < nums[mid]){
+//                end = mid - 1;
+//            } else if (target > nums[mid]) {
+//                start = mid + 1;
+//            }else{
+//                ans = mid;
+//                if(Leftside){
+//                    end = mid - 1;
+//                }else{
+//                    start = mid + 1;
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+static int floor(int [] arr , int target){
+    int start = 0;
+    int end = arr.length -1;
 
-        ans[0] = start;
-        ans[1] = end;
+    while(start <= end){
+        int mid = start + (end - start)/2;
 
-        return ans;
-    }
-
-    static int search(int [] nums, int target, boolean Leftside){
-        int ans = -1;
-        int start = 0;
-        int end = nums.length -1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(target < nums[mid]){
-                end = mid - 1;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
-            }else{
-                ans = mid;
-                if(Leftside){
-                    end = mid - 1;
-                }else{
-                    start = mid + 1;
-                }
-            }
+        if(arr[mid] == target){
+            return mid;
         }
-        return ans;
+        if(target < arr[mid+1]){
+            return mid+1;
+        }
+        return start;
     }
-
+    return -1;
+}
 }
