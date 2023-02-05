97c2e26ad71dc0a174aded1319103c77366e90a5
nitin-pandita <nitin_207013@saitm.org>
2023-02-05 23:19:01 +0530

    updated

--- Searching/BinarySearch/src/Demo1.java
+++ Searching/BinarySearch/src/Demo1.java
@@ -1,10 +1,18 @@
+import org.w3c.dom.html.HTMLAreaElement;
+
 import java.util.Arrays;
 
 public class Demo1 {
     public static void main(String[] args) {
-        int [] arr = {5,7,9,18,28,30};
-        int target = 18;
-        System.out.println((floor(arr,target)));
+//        int [] arr = {7,2,5,10,8};
+//        int piece = 2;
+//        int target = 18;
+//        System.out.println((floor(arr,target)));
+//        System.out.println(splitArray(arr,piece));
+        int [] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(arr,target));
+
     }
 //    static int[] searchRange(int[] nums, int target) {
 //        int [] ans = {-1,-1};
@@ -38,21 +46,105 @@ public class Demo1 {
 //        }
 //        return ans;
 //    }
-static int floor(int [] arr , int target){
-    int start = 0;
-    int end = arr.length -1;
-
-    while(start <= end){
-        int mid = start + (end - start)/2;
-
-        if(arr[mid] == target){
-            return mid;
-        }
-        if(target < arr[mid+1]){
-            return mid+1;
-        }
-        return start;
+//static int floor(int [] arr , int target){
+//    int start = 0;
+//    int end = arr.length -1;
+//
+//    while(start <= end){
+//        int mid = start + (end - start)/2;
+//
+//        if(arr[mid] == target){
+//            return mid;
+//        }
+//        if(target < arr[mid+1]){
+//            return mid+1;
+//        }
+//        return start;
+//    }
+//    return -1;
+//    ---------------------------------------------------
+//    static int splitArray(int [] arr,int k){
+//        int start = 0;
+//        int end = 0;
+//
+//        // for making a range
+//        for (int j : arr) {
+//            int max = Math.max(start, j); // min ans
+//            end += j;
+//        }
+//
+//        while(start < end){
+//            int mid = start + (end - start)/2;
+//            int sum = 0;
+//            int piece = 1;
+//
+//            for (int num:arr
+//                 ) {
+//                if(sum + num > mid){
+//                    // create new array
+//                    sum = num;
+//                    piece++;
+//                }else{
+//                    sum += num;
+//                }
+//            }
+//            if(piece > k){
+//                start = mid +1;
+//            }else{
+//                end = mid;
+//            }
+//        }
+//        return start;
+//----------------------------------------------------------------
+static int search(int[] arr, int target){
+        int pivot = findPivot(arr);
+        // array is not rotated;
+    if(pivot == -1){
+        return binarySearch(arr,target,0,arr.length-1);
     }
-    return -1;
+    if(arr[pivot] == target){
+        return pivot;
+    }if(target > arr[0]){
+        return binarySearch(arr,target,0,pivot-1);
+    }else{
+        return binarySearch(arr,target,pivot+1,arr.length-1);
+    }
+}
+    static int findPivot(int [] arr){
+        int start = 0;
+        int end = arr.length -1;
+
+        while(start <= end){
+            int mid = start +( end - start)/2;
+
+            if(mid < end && arr[mid] >= arr[mid +1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] <= arr[mid -1]){
+                return  mid -1;
+            }
+            if(arr[start] >= arr[mid]) {
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+
+}
+static int binarySearch(int[] arr, int target, int start, int end){
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else if(target > arr[mid]){
+                start = mid +  1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
 }
 }
