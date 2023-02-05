2b93d8e0f0b0f1d2e41f08f5af6feebb8b2a4382
nitin-pandita <nitin_207013@saitm.org>
2023-02-05 23:17:45 +0530

    Cyclic sort

--- Sorting/src/Cyclic_Sort.java
+++ Sorting/src/Cyclic_Sort.java
@@ -8,27 +8,27 @@ public class Cyclic_Sort {
     }
     static void cyclic(int[] arr){
         // method 1
-//        for (int i = 0; i < arr.length;) {
-//
-//            int index = arr[i] -1;
-//            if(arr[i] != arr[index] ){
-//                int temp = arr[i];
-//                arr[i] = arr[index];
-//                arr[index] = temp;
-//            }else{
-//                i++;
-//            }
-//        }
-        // method 2
-        int i = 0;
-        while (i < arr.length){
-            int correct = arr[i] - 1;
-            if(arr[i] != arr[correct]){
-                swap(arr,i,correct);
+        for (int i = 0; i < arr.length;) {
+
+            int index = arr[i] -1;
+            if(arr[i] != arr[index] ){
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
             }else{
                 i++;
             }
         }
+        // method 2
+//        int i = 0;
+//        while (i < arr.length){
+//            int correct = arr[i] - 1;
+//            if(arr[i] != arr[correct]){
+//                swap(arr,i,correct);
+//            }else{
+//                i++;
+//            }
+//        }
     }
 
      static void swap(int[] arr, int i, int correct) {
