cf7e52f9f1a05527748fcc8309955d344257f5f9
nitin-pandita <nitin_207013@saitm.org>
2023-02-09 12:08:11 +0530

    files

---
+++ String and String Builders/src/printfunction.java
@@ -0,0 +1,10 @@
+public class printfunction {
+    public static void main(String[] args) {
+//        System.out.println(45);
+//        System.out.println("Nitin");
+        System.out.println(new int[]{12,3,4,45});
+//
+//        String name = null;
+//        System.out.println(name);
+    }
+}
