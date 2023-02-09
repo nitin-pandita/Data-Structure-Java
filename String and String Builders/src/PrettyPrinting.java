3ebff0f84b2101609f919b1ca04aefdfdf50aa6b
nitin-pandita <nitin_207013@saitm.org>
2023-02-09 12:05:30 +0530

    Pretty Printing in Java

---
+++ String and String Builders/src/PrettyPrinting.java
@@ -0,0 +1,10 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 123.12741f;
+//        System.out.printf("Formatting the Number %.2f", a);
+//        System.out.println(Math.PI);
+            // if i just want three number of pie
+//        System.out.printf("Pie %.3f",Math.PI);
+        System.out.printf("Hi my name is %s and I am %s","Nitin","Cool");
+    }
+}
