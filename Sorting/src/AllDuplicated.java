e75b60d37d820ab99c1b22ceab229d4557f07b43
nitin-pandita <nitin_207013@saitm.org>
2023-02-09 11:54:15 +0530

    Leetcode Problem

--- Sorting/src/AllDuplicated.java
+++ Sorting/src/AllDuplicated.java
@@ -3,38 +3,32 @@ import java.util.List;
 
 public class AllDuplicated {
     public static void main(String[] args) {
-
+        int [] arr = {4,3,2,7,8,2,3,1};
+        System.out.println(findDuplicates(arr));
     }
-    static List<Integer> findDuplicates(int[] arr) {
-        List <Integer> h = new ArrayList<>();
 
-        // for iterating over the loop
-            for (int i = 0; i < arr.length;) {
-                // we will check if  index + 1 is equal to the value at the index
-
-                if(arr[i] != i + 1){
-                    int correctIndex = arr[i] -1 ;
-// if not then swap the values
-                    if(arr[i] != arr[correctIndex]){
-                        int temp = arr[i];
-                        arr[i] = arr[correctIndex];
-                        arr[correctIndex] = temp;
-                    }else{
-                        // if the value is already there mean the number is the duplicate item return it
-                        List <Integer> ans = new ArrayList<>();
-
-                        for(int j = 0; j < arr.length; j++){
-                            ans.add(arr[i]);
-                        }
-                        return ans;
-
-                    }
-                }else{
-                    // otherwise move to next element
-                    i++;
-                }
+    static List<Integer> findDuplicates(int[] nums) {
+// Cyclic sort algorithm
+        for (int i = 0; i < nums.length; ) {
+            int CorrectIndex = nums[i] - 1;
+            if (nums[i] != nums[CorrectIndex]) { // if arr[i] was equal to n then skip
+                // swap values
+                int temp = nums[i];
+                nums[i] = nums[CorrectIndex];
+                nums[CorrectIndex] = temp;
+            } else {
+                i++;
             }
-            return h;
         }
+        List<Integer> ans = new ArrayList<Integer>(); // Array list to store the missing element in list type
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {// index + 1 means if the number is at the correct index as index = value -1
+                // so number will be index + 1
+                ans.add(nums[index]); // add function of ArrayList
+
+            }
+        }
+        return ans;
     }
+}
 
